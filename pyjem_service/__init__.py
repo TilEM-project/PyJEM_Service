d08cfebe58049bd2f6e5f54803b344a6c041d0b1
Cameron Devine <cameron.devine@alleninstitute.org>
2025-05-27 12:36:56 -0700

    adding a mutex to avoid errors duo to multiple simultaneous requests to the scope

--- pyjem_service/__init__.py
+++ pyjem_service/__init__.py
@@ -3,6 +3,7 @@ import time
 import logging
 from PyJEM import TEM3
 from math import pi
+from threading import RLock
 
 
 class PyJEMService:
@@ -74,7 +75,10 @@ class PyJEMService:
         self.trans_tol = trans_tol
         self.rot_tol = rot_tol
 
-        assert TEM3.connect()
+        self.scope_lock = RLock()
+
+        with self.scope_lock:
+            assert TEM3.connect()
 
         self.eos = TEM3.EOS3()
         self.defl = TEM3.Def3()
@@ -100,102 +104,108 @@ class PyJEMService:
         self.connection.subscribe("scope.command", self.scope_callback)
 
     def motion_callback(self, msg):
-        self.was_in_motion = True
-        if msg.x is not None:
-            self.stage.SetX(msg.x)
-            self.x = msg.x
-        if msg.y is not None:
-            self.stage.SetY(msg.y)
-            self.y = msg.y
-        if msg.z is not None:
-            self.stage.SetZ(msg.z)
-            self.z = msg.z
+        with self.scope_lock:
+            self.was_in_motion = True
+            if msg.x is not None:
+                self.stage.SetX(msg.x)
+                self.x = msg.x
+            if msg.y is not None:
+                self.stage.SetY(msg.y)
+                self.y = msg.y
+            if msg.z is not None:
+                self.stage.SetZ(msg.z)
+                self.z = msg.z
 
     def rotation_callback(self, msg):
-        self.was_in_motion = True
-        if msg.angle_x is not None:
-            self.stage.SetTiltXAngle(msg.angle_x * 180 / pi)
-            self.tx = msg.angle_x
-        if msg.angle_y is not None:
-            self.stage.SetTiltYAngle(msg.angle_y * 180 / pi)
-            self.ty = msg.angle_y
+        with self.scope_lock:
+            self.was_in_motion = True
+            if msg.angle_x is not None:
+                self.stage.SetTiltXAngle(msg.angle_x * 180 / pi)
+                self.tx = msg.angle_x
+            if msg.angle_y is not None:
+                self.stage.SetTiltYAngle(msg.angle_y * 180 / pi)
+                self.ty = msg.angle_y
 
     def scope_callback(self, msg):
-        if msg.focus is not None:
-            self.eos.SetObjFocus(msg.focus - self.focus)
-        if msg.brightness is not None:
-            self.eos.SetBrightness(msg.brightness - self.brightness)
-            self.brightness = msg.brightness
-        if msg.mag is not None:
-            mag_table = self.LOWMAG_TABLE if msg.mag_mode == "LM" else self.MAG_TABLE
-            assert msg.mag in mag_table
-            retry = 3
-            while retry:
-                try:
-                    self.eos.SelectFunctionMode(self.MAG_MODES[msg.mag_mode])
-                except TEM3.TEM3Error as e:
-                    self._logger.warning("Timeout error when changing mag mode.")
-                    if retry == 1:
-                        raise e
-                finally:
-                    retry -= 1
-            self.eos.SetSelector(mag_table[msg.mag])
-        if msg.spot_size is not None:
-            self.eos.SelectSpotSize(msg.spot_size)
-        if msg.beam_offset is not None:
-            self.defl.SetCLA1(*msg.beam_offset)
-        if msg.screen is not None:
-            self.defl.SetBeamBlank(msg.screen == "down")
-        time.sleep(self.BEAM_MOVE_TIME)
-        if msg.focus is not None:
-            self.focus = msg.focus
-        if msg.brightness is not None:
-            self.brightness = msg.brightness
-        self.scope_status()
+        with self.scope_lock:
+            if msg.focus is not None:
+                self.eos.SetObjFocus(msg.focus - self.focus)
+            if msg.brightness is not None:
+                self.eos.SetBrightness(msg.brightness - self.brightness)
+                self.brightness = msg.brightness
+            if msg.mag is not None:
+                mag_table = self.LOWMAG_TABLE if msg.mag_mode == "LM" else self.MAG_TABLE
+                assert msg.mag in mag_table
+                retry = 3
+                while retry:
+                    try:
+                        self.eos.SelectFunctionMode(self.MAG_MODES[msg.mag_mode])
+                    except TEM3.TEM3Error as e:
+                        self._logger.warning("Timeout error when changing mag mode.")
+                        if retry == 1:
+                            raise e
+                    finally:
+                        retry -= 1
+                self.eos.SetSelector(mag_table[msg.mag])
+            if msg.spot_size is not None:
+                self.eos.SelectSpotSize(msg.spot_size)
+            if msg.beam_offset is not None:
+                self.defl.SetCLA1(*msg.beam_offset)
+            if msg.screen is not None:
+                self.defl.SetBeamBlank(msg.screen == "down")
+            time.sleep(self.BEAM_MOVE_TIME)
+            if msg.focus is not None:
+                self.focus = msg.focus
+            if msg.brightness is not None:
+                self.brightness = msg.brightness
+            self.scope_status()
 
     def scope_status(self):
-        self.connection.send(
-            "scope.status",
-            focus=self.focus,
-            aperture=None,
-            mag_mode="MAG" if self.eos.GetFunctionMode()[0] < 2 else "LM",
-            mag=self.eos.GetMagValue()[0],
-            tank_voltage=self.gun.GetHtCurrentValue()[0],
-            spot_size=self.eos.GetSpotSize(),
-            beam_offset=self.defl.GetCLA1(),
-            screen="down" if self.defl.GetBeamBlank() else "up",
-            brightness=self.brightness,
-        )
-        self.last_scope_status = time.time()
+        with self.scope_lock:
+            self.connection.send(
+                "scope.status",
+                focus=self.focus,
+                aperture=None,
+                mag_mode="MAG" if self.eos.GetFunctionMode()[0] < 2 else "LM",
+                mag=self.eos.GetMagValue()[0],
+                tank_voltage=self.gun.GetHtCurrentValue()[0],
+                spot_size=self.eos.GetSpotSize(),
+                beam_offset=self.defl.GetCLA1(),
+                screen="down" if self.defl.GetBeamBlank() else "up",
+                brightness=self.brightness,
+            )
+            self.last_scope_status = time.time()
 
     def stage_status(self):
-        x, y, z, tx, ty = self.stage.GetPos()
-        self.connection.send(
-            "stage.motion.status",
-            x=int(x),
-            y=int(y),
-            z=int(z),
-            in_motion=(in_motion:=self.in_motion),
-        )
-        self.connection.send(
-            "stage.rotation.status",
-            angle_x=tx * pi / 180,
-            angle_y=ty * pi / 180,
-            eucentric_height=0,
-            in_motion=in_motion,
-        )
-        self.last_stage_status = time.time()
+        with self.scope_lock:
+            x, y, z, tx, ty = self.stage.GetPos()
+            self.connection.send(
+                "stage.motion.status",
+                x=int(x),
+                y=int(y),
+                z=int(z),
+                in_motion=(in_motion:=self.in_motion),
+            )
+            self.connection.send(
+                "stage.rotation.status",
+                angle_x=tx * pi / 180,
+                angle_y=ty * pi / 180,
+                eucentric_height=0,
+                in_motion=in_motion,
+            )
+            self.last_stage_status = time.time()
 
     @property
     def in_motion(self):
-        x, y, z, tx, ty = self.stage.GetPos()
-        return any(
-            [
-                abs(s - p) > self.trans_tol
-                for s, p in zip((self.x, self.y, self.z), (x, y, z))
-            ]
-            + [abs(s - p) > self.rot_tol for s, p in zip((self.tx, self.ty), (tx, ty))]
-        )
+        with self.scope_lock:
+            x, y, z, tx, ty = self.stage.GetPos()
+            return any(
+                [
+                    abs(s - p) > self.trans_tol
+                    for s, p in zip((self.x, self.y, self.z), (x, y, z))
+                ]
+                + [abs(s - p) > self.rot_tol for s, p in zip((self.tx, self.ty), (tx, ty))]
+            )
 
     def run_once(self):
         period = 1 / 50 if (in_motion:=self.in_motion) or self.was_in_motion else 1
