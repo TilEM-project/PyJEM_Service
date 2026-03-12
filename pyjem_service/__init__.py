3bb2d1469463ef23dbbfcdce43f1a6d5c7792d29
dlbrittain <dlbrittain@gmail.com>
2026-03-12 09:01:33 -0700

    fix: attempt to reconnect indefinitely

--- pyjem_service/__init__.py
+++ pyjem_service/__init__.py
@@ -98,7 +98,7 @@ class PyJEMService:
         self.last_stage_status = time.time()
         self.last_scope_status = time.time()
 
-        self.connection = Pigeon("pyjem_service", host=host, port=port, spawn_threads=True)
+        self.connection = Pigeon("pyjem_service", host=host, port=port, spawn_threads=True, connection_timeout=None)
         self.connection.connect(username=username, password=password)
         self.connection.subscribe("stage.motion.command", self.motion_callback)
         self.connection.subscribe("stage.rotation.command", self.rotation_callback)
@@ -226,5 +226,8 @@ class PyJEMService:
 
     def run(self):
         while True:
-            self.run_once()
+            try:
+                self.run_once()
+            except Exception:
+                self._logger.warning("Error in run loop:", exc_info=True)
             time.sleep(0.01)
